235a029025086a3a054cc60e61ea19b626815003
Saad Muhamamd <s.muhammad86@gmail.com>
2020-10-03 19:55:44 +0500

    Update Api.js

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -5,12 +5,14 @@ export const Api = async () => {
     const json = await(fetch(url))
     const response = await json.json()
     const active = response.infected-(response.recovered+response.deceased)
+    const fatalityRate = ((response.deceased/response.infected)*100)
     const modifiedData = {
         infected: response.infected,
         recovered: response.recovered,
         critical: response.critical,
         deceased: response.deceased,
-        active: active
+        active: active,
+        fatality_rate: fatalityRate
     }
     return(
     modifiedData
